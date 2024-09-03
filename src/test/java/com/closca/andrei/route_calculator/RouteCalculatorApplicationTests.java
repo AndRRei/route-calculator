35bd87170b863291ef574a7742f40ce0952f20d1
Andrei <andreiclosca92@gmail.com>
2024-09-03 17:42:44 +0300

    initial commit with project structure

---
+++ src/test/java/com/closca/andrei/route_calculator/RouteCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.closca.andrei.route_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
